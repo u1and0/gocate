2955fc0160d865d8311bab8e2dc74114965c0002
u1and0 <e01.ando60@gmail.com>
2021-08-28 01:18:32 +0900

    [mod] delay 1 Microsecond
    $ go test -v
    === RUN   TestNormalLocate
    --snip--
    --- PASS: TestNormalLocate (0.02s)
    === RUN   TestParallelLocate
    --snip--
    --- PASS: TestParallelLocate (0.00s)
    PASS
    ok      speedtest/src/github.com/u1and0/gocate  0.031s

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 func main() {
@@ -13,11 +14,13 @@ func main() {
 
 func locateBin() {
 	b, _ := exec.Command("locate", "-i", "-d", "./test/bin.db", "ls").Output()
+	time.Sleep(1 * time.Microsecond)
 	fmt.Printf("%v", string(b))
 }
 
 func locateUsr() {
 	b, _ := exec.Command("locate", "-i", "-d", "./test/usr.db", "ing.hpp").Output()
+	time.Sleep(1 * time.Microsecond)
 	fmt.Printf("%v", string(b))
 }
 
