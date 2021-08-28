6d437542bc91e01b0b2fb67602e0b5409043fa89
u1and0 <e01.ando60@gmail.com>
2021-08-29 07:44:59 +0900

    [mod] go func as Anonymous function

--- main.go
+++ main.go
@@ -25,21 +25,28 @@ func main() {
 	var wg sync.WaitGroup // カウンタを宣言
 	wg.Add(2)             // カウンタの初期化
 	for _, o := range opts {
-		go Locate(o, &wg)
+		go func(o Opt) {
+			b, _ := exec.Command("locate", "-i", "-d", o.Dir, o.Word).Output()
+			out := strings.Split(string(b), "\n")
+			for _, o := range out {
+				fmt.Println(o)
+			}
+			wg.Done() // カウンタを減算
+		}(o)
 	}
 	wg.Wait() // カウンタが0になるまでブロック
 }
 
-// Locate excutes locate command
-func Locate(o Opt, wg *sync.WaitGroup) {
-	b, _ := exec.Command("locate", "-i", "-d", o.Dir, o.Word).Output()
-	out := strings.Split(string(b), "\n")
-	for _, o := range out {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Println(o)
-	}
-	wg.Done() // カウンタを減算
-}
+// // Locate excutes locate command
+// func Locate(o Opt, wg *sync.WaitGroup) {
+// 	b, _ := exec.Command("locate", "-i", "-d", o.Dir, o.Word).Output()
+// 	out := strings.Split(string(b), "\n")
+// 	for _, o := range out {
+// 		time.Sleep(1 * time.Microsecond)
+// 		fmt.Println(o)
+// 	}
+// 	wg.Done() // カウンタを減算
+// }
 
 // Nomral locate command for benchmark
 func locateBin0() {
