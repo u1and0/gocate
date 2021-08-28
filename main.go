d7ebeb775bd7c1e255b280c1b6a4e4cb71b4f38b
u1and0 <e01.ando60@gmail.com>
2021-08-29 00:10:53 +0900

    [mod] goroutine for loop

--- main.go
+++ main.go
@@ -7,37 +7,39 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
-	"time"
 )
+
+// Opt : locate command options
+type Opt struct {
+	Dir  string
+	Word string
+}
 
 func main() {
 	var wg sync.WaitGroup // カウンタを宣言
 	wg.Add(2)             // カウンタの初期化
-	go locateBin(&wg)
-	go locateUsr(&wg)
+	opts := []Opt{
+		{"./test/bin.db", "ls"},
+		{"./test/usr.db", "ing.hpp"},
+	}
+	for _, o := range opts {
+		go Locate(o, &wg)
+	}
 	wg.Wait() // カウンタが0になるまでブロック
 }
 
-func locateBin(wg *sync.WaitGroup) {
-	b, _ := exec.Command("locate", "-i", "-d", "./test/bin.db", "ls").Output()
+// Locate excutes locate command
+func Locate(o Opt, wg *sync.WaitGroup) {
+	b, _ := exec.Command("locate", "-i", "-d", o.Dir, o.Word).Output()
 	out := strings.Split(string(b), "\n")
 	for _, o := range out {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%s\n", o)
+		// time.Sleep(1 * time.Microsecond)
+		fmt.Println(o)
 	}
 	wg.Done() // カウンタを減算
 }
 
-func locateUsr(wg *sync.WaitGroup) {
-	b, _ := exec.Command("locate", "-i", "-d", "./test/usr.db", "ing.hpp").Output()
-	out := strings.Split(string(b), "\n")
-	for _, o := range out {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%s\n", o)
-	}
-	wg.Done() // カウンタを減算
-}
-
+/* Nomral locate command for benchmark
 func locateBin0() {
 	b, _ := exec.Command("locate", "-i", "-d", "./test/bin.db", "ls").Output()
 	out := strings.Split(string(b), "\n")
@@ -55,3 +57,4 @@ func locateUsr0() {
 		fmt.Printf("%s\n", o)
 	}
 }
+*/
